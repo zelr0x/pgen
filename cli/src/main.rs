c654c1ae1874e1223986b2abc4f4ecaea2da293c
zelr0x <zelr0xff@gmail.com>
2026-04-29 20:25:23 +0500

    Move gen to a lib, update deps, add zeroize, add cli arg validation

---
+++ cli/src/main.rs
@@ -0,0 +1,40 @@
+use clap::Parser;
+use pgen::{self, ExposeSecret};
+
+const MIN_RAW: i32 = 8;
+const MAX_RAW: i32 = 4096;
+const MIN: u16 = MIN_RAW as u16;
+const MAX: u16 = MAX_RAW as u16;
+
+/// Simple ASCII password generator.
+///
+/// Results consist of numbers, latin letters, hyphens and underscores.
+///
+/// Uses ChaCha12 CRNG for secure generation and low-level tricks
+/// to avoid leaving passwords in memory.
+#[derive(Parser, Debug)]
+#[command(name = "pgen", author, version, about, long_about)]
+struct Args {
+    #[arg(default_value_t = 16, value_parser=parse_number)]
+    length: u16
+}
+
+fn parse_number(s: &str) -> Result<u16, String> {
+    match s.parse::<i32>() {
+        Ok(n) if n < MIN_RAW => Err(
+            format!("Length must be at least {} characters", MIN)),
+        Ok(n) if n <= MAX_RAW => Ok(n as u16),
+        Ok(_) => Err(format!("Length must be below {} characters", MAX)),
+        Err(_) => Err("Invalid number format".to_string()),
+    }
+}
+
+fn main() {
+    let args = Args::parse();
+    let n = args.length;
+    assert!(n >= MIN && n <= MAX);
+    let p = pgen::generate(n);
+    for b in p.expose_secret() {
+        print!("{}", *b as char);
+    }
+}
---
+++ lib/src/lib.rs
@@ -0,0 +1,27 @@
+use rand::{
+    self, CryptoRng, Rng
+};
+use rand_distr::{Distribution, uniform::Uniform};
+use secrecy::SecretSlice;
+
+pub use secrecy::ExposeSecret;
+
+const ALPHABET: &'static[u8] = b"0123456789-_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+pub fn generate(n: u16) -> SecretSlice<u8> {
+    let mut rng = rand::rng();
+    generate_with_crypto(&mut rng, n)
+}
+
+pub fn generate_with_crypto<R: Rng + CryptoRng>(rng: &mut R, n: u16) -> SecretSlice<u8> {
+    generate_with(rng, n)
+}
+
+pub fn generate_with<R: Rng>(rng: &mut R, n: u16) -> SecretSlice<u8> {
+    let s = Uniform::new(0, ALPHABET.len()).unwrap()
+        .sample_iter(rng)
+        .take(n as usize)
+        .map(|i| ALPHABET[i])
+        .collect::<Vec<u8>>();
+    SecretSlice::new(s.into())
+}
